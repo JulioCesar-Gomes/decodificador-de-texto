6ef5280f700a072d08add75896ab017f82ab7a1e
Julio Cesar <juliocesar_dev@hotmail.com>
2024-08-27 01:28:07 -0300

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,25 @@
+
+Decodificador de Texto - Desafio da Alura/ONE Next Education
+
+Projetado e desenvolvido como desafio, idealizado pela Alura em conjunto com o programa ONE Next Education. Sua função permitir criptografar e descriptografar textos, tendo também um layout simples e intuitivo, sendo de fácil uso do mesmo.
+
+Principal Objetivo:
+- Trazer maior segurança para seus textos, criptografando-os, tendo como resultado o sigilo de seu conteúdo.
+
+Funcionalidades:
+- Encriptação de texto: Converte mensagens inseridas em mensagens codificadas.
+- Descriptação de texto: Decodifica as mensagens antes codificadas.
+- Copiar: efetua a cópia do texto, tanto criptografado quanto descriptografado.
+
+Tecnologias:
+1- JavaScript: Lógica de Programação.
+2- HTML: Estrutura e marcação.
+3- Estilização da Interface.  
+
+Como os usuários podem utilizá-lo:
+1- O usuário deverá inserir o texto no campo indicado (na caixa à esquerda).
+2- Clicando no botão "Criptografar" o texto inserido será criptografado e aparecerá no campo indicado (na caixa à direita).
+2- Clicando no botão "Descriptografar" o texto inserido será descriptografado e aparecerá no campo indicado (na caixa à direita).
+3- Clicando no botão "Copiar" o texto na caixa à direita seja criptado ou descriptado será copiado para área de transferência.
+
+Autores do projeto: Desenvolvido por Julio Cesar.
---
+++ script.js
@@ -0,0 +1,40 @@
+//Combinação de normalize e replace - Resumindo decompõe os caracteres com acentos, separa-os e retira-os e substitui por "vazio"
+function retirarAcentuacao(texto) {
+    return texto.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+}
+
+function criptografar() {
+    let inputText = document.getElementById("inputText").value;
+    inputText = retirarAcentuacao(inputText);
+
+    //Replace - substitui elementos/partes de uma string por outra. 
+    let outputText = inputText.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober")
+        .replace(/u/g, "ufat").replace(/[^a-z]/g, "");
+    document.getElementById("outputText").value = outputText;
+}
+
+function descriptografar() {
+    let inputText = document.getElementById("inputText").value;
+    inputText = retirarAcentuacao(inputText);
+    let outputText = inputText.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o")
+        .replace(/ufat/g, "u").replace(/[^a-z]/g, "");// A letra g indica que todas as ocorrencias serão substituidas, (globalmente).
+
+    //Seleciona Campo do HTML e retorna com novo valor atribuido (criptografado).
+    document.getElementById("outputText").value = outputText;
+}
+
+//Remove automaticamentes elementos indesejados do inputText especificados na funcao retirarAcentuacao()
+const botaoCopiador = document.getElementById("botaoCopiador");
+botaoCopiador.addEventListener('click', () => {
+    navigator.clipboard.writeText(document.getElementById("outputText").value)
+        .then(() => {
+            alert("Copiado para área de transferência");
+        });
+});
+
+document.getElementById("inputText").addEventListener("input", function () {
+    this.value = retirarAcentuacao(this.value)
+});
+document.getElementById("outputText").addEventListener("input", function () {
+    this.value = retirarAcentuacao(this.value)
+});
